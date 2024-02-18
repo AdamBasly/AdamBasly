abb005cd1b029cd78421428ed5a6f230e9510a70
AdamBasly <159331030+AdamBasly@users.noreply.github.com>
2024-02-18 17:20:11 +0100

    Update code_1.py
    The English Extension update is here, but it is under development.

--- code_1.py
+++ code_1.py
@@ -28,4 +28,29 @@ if language==1:
         elif eingabe=="eat something" or "etwas essen":
             print("https://mama-pizza.de/")
         eingabe=input("Was willst du jetzt tun?")
-print("Du hast die KI abgebrochen.")
+elif language==2:
+    print("You have choosed English")
+    eingabe=input("What do you want to do")
+    while True:
+        if eingabe=="help" or eingabe=="hilfe":
+            print("1=learn with the mouse. 2=more about you")
+            abfrage1=input("Do you want to abort the KI?")
+            if abfrage1=="Yes":
+                break
+        elif eingabe=="1":
+            learn=input("What do you want to learn?")
+            if learn=="Python":
+                print("https://www.udemy.com/")
+                abfrage2=input("Do you want to abort the KI?")
+                if abfrage2=="Ja":
+                    break
+        elif eingabe=="2":
+            print("I'm the KI of adamrun155")
+        elif eingabe=="Github"or eingabe=="github":
+            print("https://github.com/")
+        elif eingabe=="Visual Studio Code" or eingabe=="visual studio code" or eingabe=="install coding application":
+            print("https://code.visualstudio.com/")
+        elif eingabe=="eat something" or "etwas essen":
+            print("https://mama-pizza.de/")
+        eingabe=input("What do you want to do?")
+print("Du hast die KI abgebrochen./You have aborted the KI.")
